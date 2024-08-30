debe904ef2b491645b8e18c5c3fa2277f07ce8af
joaovitoreggers <joaoguchert195@gmail.com>
2024-08-30 07:59:20 -0300

    add products business rules, and front-end

---
+++ products/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from . import models
+
+class ProductForm(forms.ModelForm):
+    
+    class Meta:
+        model = models.Product
+        fields = ['title', 'category', 'brand', 'description',
+                   'serie_number', 'cost_price', 'selling_price',]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'brand': forms.Select(attrs={'class': 'form-control',}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'serie_number': forms.TextInput(attrs={'class': 'form-control',}),
+            'cost_price': forms.NumberInput(attrs={'class': 'form-control', }),
+            'selling_price': forms.NumberInput(attrs={'class': 'form-control',}),
+        }
+        lables = {
+            'title': 'Título',
+            'category': 'Categoria',
+            'quantity': 'Quantidade',
+            'brand': 'Marca',
+            'serie_number': 'Número de Série',
+            'cost_price': 'Custo de Compra',
+            'selling_price': 'Custo de Venda',
+            'description': 'Descrição',
+        }
--- products/views.py
+++ products/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from . import models, forms
 
-# Create your views here.
+class ProductListView(ListView):
+    model = models.Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.GET.get('title')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
+    
+class ProductCreateView(CreateView):
+    model = models.Product
+    template_name = 'product_create.html'
+    form_class = forms.ProductForm
+    success_url = reverse_lazy('product_list')
+
+class ProductDetailView(DetailView):
+    model = models.Product
+    template_name = 'product_detail.html'
+    context_object_name = 'products'
+
+class ProductUpdateView(UpdateView):
+    model = models.Product
+    template_name = 'product_update.html'
+    form_class = forms.ProductForm
+    success_url = reverse_lazy('product_list')
+    context_object_name = 'products'
+
+class ProductDeleteView(DeleteView):
+    model = models.Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
+    context_object_name = 'products'
